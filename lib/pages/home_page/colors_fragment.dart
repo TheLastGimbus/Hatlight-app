9be2f9b97ef3c354ccc7146e4292f2c2b4edbfed
TheLastGimbus <mateusz.soszynski@tuta.io>
2020-07-08 02:14:54 +0200

    Add color picker to interact with setColor

--- lib/pages/home_page/colors_fragment.dart
+++ lib/pages/home_page/colors_fragment.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_circle_color_picker/flutter_circle_color_picker.dart';
+import 'package:hatlight/providers/bt_provider.dart';
+import 'package:provider/provider.dart';
 
 class ColorsFragment extends StatefulWidget {
   @override
@@ -6,8 +11,34 @@ class ColorsFragment extends StatefulWidget {
 }
 
 class _ColorsFragmentState extends State<ColorsFragment> {
+  var _color = Colors.white;
+  var _sending = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('Fragment'));
+    var bt = Provider.of<BTProvider>(context);
+    return Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: <Widget>[
+          CircleColorPicker(
+            onChanged: (color) async {
+              _color = color;
+              if (_sending) return;
+              _sending = true;
+              bt.setColor(_color);
+              await Future.delayed(Duration(milliseconds: 30));
+              _sending = false;
+            },
+          ),
+          RaisedButton(
+            child: Text('Send'),
+            onPressed: () {
+              bt.setColor(_color);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
